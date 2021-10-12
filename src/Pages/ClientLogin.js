4b50b18f094fd883ed3cd1ceec6f1b122a568295
Pemith <pemithr@gmail.com>
2021-10-12 23:20:32 +0530

    Client Login Route Added

--- src/Pages/ClientLogin.js
+++ src/Pages/ClientLogin.js
@@ -1,24 +1,54 @@
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
+import { useState } from 'react';
 import '../CSS/ClientLogin.css';
+import axios from 'axios';
 
 const ClientLogin = () => {
+
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("");
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+
+        axios.post("http://localhost:3900/api/clientauth/",{
+            email:email,
+            password:password
+        })
+        .then(
+            (response)=>{
+                console.log(response);
+                console.log("Logged In Successfully");
+            },
+            (error) =>{
+                console.log(error);
+            }
+        )
+        .catch((err)=>{
+            console.log(err);
+        })
+    }
     return ( 
         <div className="login-img">
             <Navbar/>
                 <div className="boxlog">
                     <div className="formstyle">
                     <h1>Login</h1>
-                        <form>
+                        <form onSubmit={handleSubmit}>
                             <label>Email</label>
                             <input 
                                 type="email"
                                 required
+                                value={email}
+                                onChange={e => setEmail(e.target.value)}
                             />
                             <label>Password</label>
                             <input 
                                 type="password"
                                 required
+                                value={password}
+                                onChange={e=>setPassword(e.target.value)}
                             />
                             <button>Login</button>
                         </form>
